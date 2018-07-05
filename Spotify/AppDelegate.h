ef9ebca78c8669ef1ef33df34ce916b82aec613a
Gokhan Gultekin <gokhangultekin07@hotmail.com>
2018-07-05 20:56:04 +0300

    First commit

---
+++ Spotify/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Spotify
+//
+//  Created by Gokhan Gultekin on 5.07.2018.
+//  Copyright © 2018 Gokhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Spotify/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Spotify
+//
+//  Created by Gokhan Gultekin on 5.07.2018.
+//  Copyright © 2018 Gokhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
